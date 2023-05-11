1a07771c076394a8fa2d2bb6b9d1919c81538360
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-11 19:14:37 +0200

    Added WeatherContainer

--- react-electron-ui/src/App.tsx
+++ react-electron-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import WeatherContainer from "./components/WeatherContainer";
+import WeatherContainer from "./components/Weather/WeatherContainer";
 
 function App() {
   return (
@@ -9,9 +9,11 @@ function App() {
       <header className="App-header"></header>
 
       <div className='container'>
+
         <div className='quarter'>
           <WeatherContainer></WeatherContainer>
         </div>
+
         <div className='quarter'>content 2 </div>
         <div className='quarter'>content 3 </div>
         <div className='quarter'>content 4</div>
---
+++ react-electron-ui/src/components/Weather/WeatherContainer.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from "react";
+import "./WeatherContainer.css";
+
+const WeatherContainer: FunctionComponent = () => {
+    return (
+        <div className="weatherContainer">
+            <div className="currentWeatherContainer">
+
+
+            </div>
+
+            <div className="forecast">
+
+            </div>
+
+        </div>)
+}
+
+export default WeatherContainer;
+
