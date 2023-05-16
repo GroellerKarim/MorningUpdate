67b0f0f446e4f4193a1a53864716e9e152ff8e84
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-16 23:05:11 +0200

    Trying to get some weather data

--- react-electron-ui/src/components/Weather/WeatherContainer.tsx
+++ react-electron-ui/src/components/Weather/WeatherContainer.tsx
@@ -1,11 +1,33 @@
-import React, { FunctionComponent } from "react";
+import { AnyRecord } from "dns";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import "./WeatherContainer.css";
 
-const WeatherContainer: FunctionComponent = () => {
+const WeatherContainer: FunctionComponent = () => { 
+   const [data, setData] = useState<AnyRecord | null>(null);
+
+   useEffect(() => {
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Vienna&limit=5&appid=')
+      .then((response) => {
+        if (!response.ok) {
+                    console.log(response);
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []); 
+
     return (
         <div className="weatherContainer">
             <div className="currentWeatherContainer">
 
+                <div>
+
+                
+                </div>
 
             </div>
 
@@ -17,4 +39,3 @@ const WeatherContainer: FunctionComponent = () => {
 }
 
 export default WeatherContainer;
-
