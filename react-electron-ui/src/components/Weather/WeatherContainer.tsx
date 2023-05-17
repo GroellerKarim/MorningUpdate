4305291090b643120dc6c8e52e47e82fa51e9731
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-18 01:41:46 +0200

    Played around with ZAMG requests

--- react-electron-ui/src/components/Weather/WeatherContainer.tsx
+++ react-electron-ui/src/components/Weather/WeatherContainer.tsx
@@ -1,33 +1,38 @@
 import { AnyRecord } from "dns";
+import { url } from "inspector";
 import React, { FunctionComponent, useEffect, useState } from "react";
+import { URL, URLSearchParams } from "url";
 import "./WeatherContainer.css";
 
-const WeatherContainer: FunctionComponent = () => { 
-   const [data, setData] = useState<AnyRecord | null>(null);
+const WeatherContainer: FunctionComponent = () => {
+    const [data, setData] = useState<AnyRecord | null>(null);
 
-   useEffect(() => {
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Vienna&limit=5&appid=')
-      .then((response) => {
-        if (!response.ok) {
-                    console.log(response);
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => setData(data))
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []); 
+    const url = new URL('/v1/grid/current/11035');
+    url.search = new URLSearchParams({
+        parameters: ['RR', 'TLMIN', 'TLMAX']
+    }).toString();
+
+    useEffect(() => {
+        fetch(url.toString())
+            .then((response) => {
+                console.log(response);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => { 
+                console.log(data);
+                setData(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
     return (
         <div className="weatherContainer">
             <div className="currentWeatherContainer">
-
-                <div>
-
-                
-                </div>
 
             </div>
 
